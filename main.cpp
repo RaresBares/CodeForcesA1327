8887ea9c3a411a017e41f599e9d84f794dd476ae
Rares <>
2020-04-07 20:43:24 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--){
+        int a,b;
+        cin >> a >> b;
+
+        if((a+b)%2 == 0){
+            cout << "YES" << "\n";
+        } else{
+            cout << "NO"<< "\n";
+        }
+
+    }
+    return 0;
+}
